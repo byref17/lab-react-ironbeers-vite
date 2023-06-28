778ff9ddffe45cf60dce76b7a66ecb48886bc92b
byref17 <124827023+byref17@users.noreply.github.com>
2023-06-28 19:20:40 +0200

    iteration 4

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,23 @@
-function AllBeersPage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+function AllBeersPage() {
+    const [allBeers, setAllBeers] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://ih-beers-api2.herokuapp.com/beers")
+            .then((response) => {
+                console.log('response.data', response.data);
+                setAllBeers(response.data);
+            })
+    }), [];
+
+    return (
+        <div>
+
+        </div>
+
+    )
+}
 export default AllBeersPage;
